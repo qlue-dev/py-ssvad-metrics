82facb6f9ff258704f1e3fcea8231658b3116831
Yahya <yahya@qlue.id>
2021-04-08 18:25:55 +0700

    bugfix

--- notebooks/ucsd_ped_ss_cvt.py
+++ notebooks/ucsd_ped_ss_cvt.py
@@ -32,8 +32,8 @@ def main(args):
         }
         annos_df = pd.read_csv(
             anno_fpath, sep=" ", index_col=False, header=None, names=["filename", "T", "x", "y", "w", "h"])
-        annos_df["frame_id"] = annos_df.apply(
-            lambda x: int(os.path.splitext(x["filename"])[0]))
+        annos_df["frame_id"] = annos_df["filename"].apply(
+            lambda x: int(os.path.splitext(x)[0]))
         annos_df = annos_df.sort_values("frame_id")
         annos_df_grouped = annos_df.groupby("frame_id")
         for frame_id in range(vad_anno["frames_count"]):
