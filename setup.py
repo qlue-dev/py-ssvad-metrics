46dd24a75a665d0d41cf0347234b927a9b690485
Yahya <yahya@qlue.id>
2021-04-13 09:19:08 +0700

    Add setup

---
+++ setup.py
@@ -0,0 +1,38 @@
+
+import os
+from setuptools import find_packages, setup
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+with open(os.path.join(here, 'ssvad_metrics/VERSION'), encoding='utf-8') as f:
+    version = f.read()
+
+setup(
+    name='py-ssvad_metrics',
+    version=version,
+    description='Single Scene Video Anomaly Detection Metrics',
+    long_description=long_description,
+    url='https://qlue.ai',
+    author='PT Qlue Performa Indonesia',
+    author_email='developer@qlue.id',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence'
+    ],
+    packages=find_packages(),
+    package_data={
+        "": ["VERSION"],
+    },
+    python_requires='>=3.6,<4',
+    install_requires=[
+        'numpy>=1.18',
+        'typing_extensions>=3.7.4',
+        'pytest>=6.2.3',
+        'scikit-learn>=0.24.1',
+        'scipy>=1.5.4',
+        'pydantic>=1.8.1',
+        'pandas>=1.1.5']
+)
--- ssvad_metrics/__init__.py
+++ ssvad_metrics/__init__.py
@@ -1,1 +1,8 @@
+"""
+Single Scene Video Anomaly Detection Metrics
+"""
+import pkgutil
+
 from ssvad_metrics import criteria, data_schema, metrics, utils
+
+__version__ = pkgutil.get_data("ssvad_metrics", "VERSION").decode("utf-8")
