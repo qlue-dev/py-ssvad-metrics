4bcc7459609591cf60fd921527d752b7ffb8c5ed
Yahya <yahya@qlue.id>
2021-08-25 08:45:32 +0700

    Add test script

--- tests/evaluate_test.py
+++ tests/evaluate_test.py
@@ -1,3 +1,4 @@
+import numpy as np
 import ssvad_metrics
 
 
@@ -15,3 +16,15 @@ def test_accumulated_evaluate_gt_pred():
         gt_name_suffix="_gt",
         pred_name_suffix="_pred")
     print(result)
+
+
+def test_allclose():
+    result = ssvad_metrics.metrics.evaluate(
+        "tests/gt_examples/Test001_gt.json",
+        "tests/pred_examples/Test001_pred.json")
+    result_accum = ssvad_metrics.metrics.accumulated_evaluate(
+        "tests/gt_examples",
+        "tests/pred_examples",
+        gt_name_suffix="_gt",
+        pred_name_suffix="_pred")
+    assert np.allclose(list(result.values()), list(result_accum.values()))
